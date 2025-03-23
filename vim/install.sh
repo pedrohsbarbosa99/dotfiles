ef67007e699acbeaa4fb12bfa1a25e4c12dc9a23
Pedro Barbosa <pedrohsbarbosa99@gmail.com>
2025-03-23 16:31:10 -0300

    fix vim install sh

--- vim/install.sh
+++ vim/install.sh
@@ -1,10 +1,19 @@
-if [ -f "$HOME/.vim/autoload/plug.vim" ] || [ -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
+#!/bin/sh
+
+VIM_PLUG_VIM="$HOME/.vim/autoload/plug.vim"
+VIM_PLUG_NVIM="$HOME/.local/share/nvim/site/autoload/plug.vim"
+
+if [ -f "$VIM_PLUG_VIM" ] || [ -f "$VIM_PLUG_NVIM" ]; then
     echo "vim-plug already installed, continue..."
 else
-    echo "installing vim-plug.."
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo "installing vim-plug..."
+
+    curl -fLo "$VIM_PLUG_VIM" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    curl -fLo "$VIM_PLUG_NVIM" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+curl -Lo ~/.vimrc https://raw.githubusercontent.com/pedrohsbarbosa99/dotfiles/main/vim/vimrc
 
-curl -Flo ~/.vimrc https://github.com/pedrohsbarbosa99/dotfiles/vim/vimrc
